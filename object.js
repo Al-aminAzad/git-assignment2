3eb7a9fc726440048d07352845cc299abf043ea7
Al-aminAzad <alaminazad240@gmail.com>
2021-01-12 23:55:05 +0600

    Loop on objects (ES5)

---
+++ object.js
@@ -0,0 +1,13 @@
+let obj ={
+    Bangladesh:'Dhaka',
+    India:'Delhi',
+    Nepal:'Kathmandu',
+    Afganisthan:'Kabul',
+    Thailand:'Bangkok'
+};
+// let capitals = Object.values(obj);
+// console.log(capitals);
+//ES5 
+Object.keys(obj).map(function(country){
+    console.log(`The capital of ${country} is= ${obj[country]}`);
+})
